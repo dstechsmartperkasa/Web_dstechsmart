a1199285ddfb56af6d1da2c4ce9090d2bf86a58b
dstechsmartperkasa <dstechsmartperkasa@gmail.com>
2026-03-28 17:58:20 +0700

    Create Express.js server configuration

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/api', require('./routes/api'));
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).render('404', { title: 'Page Not Found' });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('error', { 
+    title: 'Server Error',
+    message: err.message 
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 DSTech Smart Perkasa website running on http://localhost:${PORT}`);
+});
